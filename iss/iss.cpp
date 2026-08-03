25c8f6c13ed14203cd3e199a04f3621e6388f5df
Pavel-Sotelo <pavelsote990@gmail.com>
2026-08-03 04:43:49 -0700

    ISS: add BEQ, BNE and JAL logic

--- iss/iss.cpp
+++ iss/iss.cpp
@@ -16,7 +16,7 @@ typedef struct {
 //decode slicing for all instruction formats
 typedef struct {
 
-    uint32_t opcode, rd, funct3, rs1, rs2, funct7;
+    uint32_t opcode, rd, funct3, rs1, rs2, funct7, current_pc;
     int32_t imm;    
 
 } DecodeInstruction;
@@ -25,17 +25,25 @@ typedef struct {
 //prototypes
 void load_program_add_5_3(CPUState *cpu);
 void load_program_6_instr(CPUState *cpu);
+void load_program_first_loop(CPUState *cpu);
+
 uint32_t fetch(CPUState *cpu);
 uint32_t decode_opcode(uint32_t instruction);
+
 void decode_r_type(uint32_t instruction, DecodeInstruction *decode);
 void decode_i_type(uint32_t instruction, DecodeInstruction *decode);
 void decode_s_type(uint32_t instruction, DecodeInstruction *decode);
+void decode_b_type(uint32_t instruction, DecodeInstruction *decode);
 void decode_u_type(uint32_t instruction, DecodeInstruction *decode);
+void decode_j_type(uint32_t instruction, DecodeInstruction *decode);
+
 void execute_r_type(DecodeInstruction *decode, CPUState *cpu);
 void execute_i_type_load(DecodeInstruction *decode, CPUState *cpu);
-void execute_s_type(DecodeInstruction *decode, CPUState *cpu);
-void execute_u_type_lui(DecodeInstruction *decode, CPUState *cpu);
 void execute_i_type_arithmetic(DecodeInstruction *decode, CPUState *cpu);
+void execute_s_type(DecodeInstruction *decode, CPUState *cpu);
+void execute_b_type(DecodeInstruction *decode, CPUState *cpu);
+void execute_u_type_lui(DecodeInstruction *decode, CPUState *cpu);
+void execute_j_type(DecodeInstruction *decode, CPUState *cpu);
 
 
 
@@ -55,7 +63,7 @@ int main ()
 
     //load the program (hex instructions)
 
-    load_program_6_instr(&cpu_state);
+    load_program_first_loop(&cpu_state);
 
     for(uint32_t i = 0; i < cpu_state.program_size; i++)
     {
@@ -67,6 +75,9 @@ int main ()
     cpu_state.pc = 0;
     while(cpu_state.pc < cpu_state.program_size * 4)
     {
+
+        //capture current pc before incrementing it (for branch/jump instructions)
+        decode_instruction.current_pc = cpu_state.pc; 
 
 
         //Fetch
@@ -106,11 +117,28 @@ int main ()
                 , decode_instruction.opcode, decode_instruction.imm, decode_instruction.funct3, decode_instruction.rs1, decode_instruction.rs2);               
             break;
             
+
+            //B-Type
+            case 0x63:
+                decode_b_type(instruction, &decode_instruction);
+                printf("\nDecode B-Type completed. - opcode = %d - imm = %d - funct3 = %d - rs1 = x%d - rs2 = x%d\n"
+                , decode_instruction.opcode, decode_instruction.imm, decode_instruction.funct3, decode_instruction.rs1, decode_instruction.rs2);               
+            break;
+
+
             //U-Type
             case 0x37:
                 decode_u_type(instruction, &decode_instruction);
                 printf("\nDecode U-Type completed. - opcode = %d - rd = x%d - imm = %d\n"
                 , decode_instruction.opcode, decode_instruction.rd, decode_instruction.imm);               
+            break; 
+
+
+            //J-Type
+            case 0x6F:
+                decode_j_type(instruction, &decode_instruction);
+                printf("\nDecode J-Type completed. - opcode = %d - rd = x%d - imm = %d\n"
+                , decode_instruction.opcode, decode_instruction.rd, decode_instruction.imm);              
             break;            
 
 
@@ -121,10 +149,9 @@ int main ()
 
         }
 
-
-        //we put the pc incrementer here, in case an execute wants to overwrite it (branch)
+        
+        //pc normal instruction increment (no branch/jump instructions)
         cpu_state.pc += 4; 
-
 
 
         //Execute
@@ -156,11 +183,34 @@ int main ()
                 printf("dmem[%u] now holds the 32-bit number of rs2 (%x)\n", cpu_state.regs[decode_instruction.rs1] + decode_instruction.imm, cpu_state.regs[decode_instruction.rs2]);                   
             break;
 
+
+            //B-Type (only beq, bne)
+            case 0x63:
+                execute_b_type(&decode_instruction, &cpu_state);
+
+                if (cpu_state.pc == decode_instruction.current_pc + 4)
+                {
+                    printf("branch not taken: x%d = %u, x%d = %u - PC = %u\n",
+                        decode_instruction.rs1, cpu_state.regs[decode_instruction.rs1], decode_instruction.rs2, cpu_state.regs[decode_instruction.rs2], cpu_state.pc);
+                }
+                else
+                {
+                    printf("branch taken: x%d = %u, x%d = %u - PC = %u\n", decode_instruction.rs1, cpu_state.regs[decode_instruction.rs1], decode_instruction.rs2, cpu_state.regs[decode_instruction.rs2], cpu_state.pc);
+                }                  
+            break;
+
+
             //U-Type (lui)
             case 0x37:
                 execute_u_type_lui(&decode_instruction, &cpu_state);
                 printf("rd = x%d now holds upper immediate: %u\n", decode_instruction.rd, cpu_state.regs[decode_instruction.rd]);                   
             break;            
+
+            //J-Type (jal)
+            case 0x6F:
+                execute_j_type(&decode_instruction, &cpu_state);
+                printf("jal: x%d = %u (return address) is PC = %u\n", decode_instruction.rd, cpu_state.regs[decode_instruction.rd], cpu_state.pc);
+            break;
 
 
             default:
@@ -182,6 +232,17 @@ int main ()
         //hardwire x0 to 0 always
         cpu_state.regs[0] = 0;    
     }
+
+        //Print each final register state at the end of the loop
+        printf("\n\nFinal register state:\n");
+            for (int i = 0; i < 32; i++)
+                printf("x%d = %u\n", i, cpu_state.regs[i]);
+
+        //Set txt file printing each final register 32-bit number (co-simulation)        
+        FILE *f = fopen("iss_regs_first_loop.txt", "w");
+        for (int i = 0; i < 32; i++)
+            fprintf(f, "x%d = %08x\n", i, cpu_state.regs[i]);
+        fclose(f);                
 
 return 0;
 }
@@ -230,6 +291,27 @@ return 0;
 
 
 
+    //load program_first_loop to the instruction memory
+    void load_program_first_loop(CPUState *cpu)
+    {
+
+        cpu->imem[0] = 0x00000093;     
+        cpu->imem[1] = 0x00000113;
+        cpu->imem[2] = 0x00400193;
+        cpu->imem[3] = 0x00000013;    
+        cpu->imem[4] = 0x00000013;
+        cpu->imem[5] = 0x003080b3;
+        cpu->imem[6] = 0x00110113;
+        cpu->imem[7] = 0x00000013;
+        cpu->imem[8] = 0x00000013;
+        cpu->imem[9] = 0xfe3118e3;
+
+        //store the number of instructions
+        cpu->program_size = 10;
+    }
+
+
+
     //fetch
     uint32_t fetch(CPUState *cpu)
     {
@@ -244,7 +326,7 @@ return 0;
 
 
 
-    //decode an r_type instruction
+    //decode an R_type instruction
     void decode_r_type(uint32_t instruction, DecodeInstruction *decode)
     {
         //opcode
@@ -282,13 +364,17 @@ return 0;
 
 
 
-    //decode an s_type instruction
+    //decode an S_type instruction
     void decode_s_type(uint32_t instruction, DecodeInstruction *decode)
     {
         //opcode
         decode->opcode = instruction & 0x7F;
         //imm
-        decode->imm = (int32_t)( ( ((instruction >> 7) & 0x1F) | ((instruction >> 20) & 0xFE0) ) << 20 ) >> 20;              
+        decode->imm = (int32_t)(
+            ( ((instruction >> 25) & 0x7F) << 5
+            | ((instruction >>  7) & 0x1F) 
+            ) << 20 ) >> 20;
+
         //funct3
         decode->funct3 = (instruction >> 12) & 0x7;
         //rs1
@@ -300,7 +386,31 @@ return 0;
 
 
 
-    //decode a u_type instruction
+    //decode a B_type instruction
+    void decode_b_type(uint32_t instruction, DecodeInstruction *decode)
+    {
+        //opcode
+        decode->opcode = instruction & 0x7F;
+        //imm
+        decode->imm = (int32_t)(
+            ( ((instruction >> 31) & 0x1)  << 12
+            | ((instruction >> 7) & 0x1)   << 11
+            | ((instruction >> 25) & 0x3F) << 5
+            | ((instruction >> 8) & 0xF)   << 1          
+            ) << 19 ) >> 19; 
+
+        //funct3
+        decode->funct3 = (instruction >> 12) & 0x7;
+        //rs1
+        decode->rs1 = (instruction >> 15) & 0x1F;
+        //rs2
+        decode->rs2 = (instruction >> 20) & 0x1F;
+   
+    }  
+
+
+
+    //decode an U_type instruction
     void decode_u_type(uint32_t instruction, DecodeInstruction *decode)
     {
         //opcode
@@ -313,7 +423,26 @@ return 0;
 
 
 
-    //execute an r_type instruction
+    //decode a J_type instruction
+    void decode_j_type(uint32_t instruction, DecodeInstruction *decode)
+    {
+        //opcode
+        decode->opcode = instruction & 0x7F;
+        //rd
+        decode->rd = (instruction >> 7) & 0x1F;
+        //imm
+        decode->imm = (int32_t)(
+            ( ((instruction >> 31) & 0x1)   << 20
+            | ((instruction >> 12) & 0xFF)  << 12
+            | ((instruction >> 20) & 0x1)   << 11
+            | ((instruction >> 21) & 0x3FF) << 1          
+            ) << 11 ) >> 11; 
+   
+    }  
+
+
+
+    //execute an R_type instruction
     void execute_r_type(DecodeInstruction *decode, CPUState *cpu)
     {
         switch(decode->funct3)
@@ -425,45 +554,6 @@ return 0;
 
 
 
-    //execute an S_type instruction
-    void execute_s_type(DecodeInstruction *decode, CPUState *cpu)
-    {
-        switch(decode->funct3)
-        {
-            //sw
-            case 0x2: {
-                        uint32_t address = decode->imm + cpu->regs[decode->rs1];
-
-                        if(address + 3 < 1024) {
-
-                            cpu->dmem[address] = cpu->regs[decode->rs2]           & 0xFF;
-                            cpu->dmem[address + 1] = (cpu->regs[decode->rs2] >>  8) & 0xFF;
-                            cpu->dmem[address + 2] = (cpu->regs[decode->rs2] >> 16) & 0xFF;
-                            cpu->dmem[address + 3] = (cpu->regs[decode->rs2] >> 24) & 0xFF; 
-
-                        } else 
-                        {
-                            printf("write out of bounds (+1024) at address %u\n", address); return;      
-                        }  
-            break;
-            }
-            default:
-                printf("funct3 S-type doesn't match any instruction - Error."); 
-            return;
-
-        }
-    }
-
-
-
-    //execute a u_type lui instruction
-    void execute_u_type_lui(DecodeInstruction *decode, CPUState *cpu)
-    {
-        cpu->regs[decode->rd] = decode->imm;    
-    }
-    
-    
-
     //execute an I_type arithmetic instruction
     void execute_i_type_arithmetic(DecodeInstruction *decode, CPUState *cpu)
     {
@@ -528,4 +618,79 @@ return 0;
             break;     
 
         }
+    }
+
+
+
+    //execute an S_type instruction
+    void execute_s_type(DecodeInstruction *decode, CPUState *cpu)
+    {
+        switch(decode->funct3)
+        {
+            //sw
+            case 0x2: {
+                        uint32_t address = decode->imm + cpu->regs[decode->rs1];
+
+                        if(address + 3 < 1024) {
+
+                            cpu->dmem[address] = cpu->regs[decode->rs2]             & 0xFF;
+                            cpu->dmem[address + 1] = (cpu->regs[decode->rs2] >>  8) & 0xFF;
+                            cpu->dmem[address + 2] = (cpu->regs[decode->rs2] >> 16) & 0xFF;
+                            cpu->dmem[address + 3] = (cpu->regs[decode->rs2] >> 24) & 0xFF; 
+
+                        } else 
+                        {
+                            printf("write out of bounds (+1024) at address %u\n", address); return;      
+                        }  
+            break;
+            }
+            default:
+                printf("funct3 S-type doesn't match any instruction - Error."); 
+            return;
+
+        }
+    }
+
+
+
+    //execute a B_type (only beq/bne) instruction
+    void execute_b_type(DecodeInstruction *decode, CPUState *cpu)
+    {
+
+        switch(decode->funct3)
+        {
+            //beq
+            case 0x0:
+                if (cpu->regs[decode->rs1] == cpu->regs[decode->rs2])
+                    cpu->pc = decode->current_pc + decode->imm;
+            break;
+
+            //bne
+            case 0x1:
+                if (cpu->regs[decode->rs1] != cpu->regs[decode->rs2])
+                    cpu->pc = decode->current_pc + decode->imm;                
+            break;
+
+            default:
+                printf("funct3 B-type doesn't match any instruction (beq/bne) - Error."); 
+            return;
+        }        
+
+    }
+
+
+
+    //execute a U_type lui instruction
+    void execute_u_type_lui(DecodeInstruction *decode, CPUState *cpu)
+    {
+        cpu->regs[decode->rd] = decode->imm;    
+    }
+    
+
+
+    //execute a J_type (jal) instruction
+    void execute_j_type(DecodeInstruction *decode, CPUState *cpu)
+    {
+        cpu->regs[decode->rd] = decode->current_pc + 4;
+        cpu->pc = decode->current_pc + decode->imm;
     }
